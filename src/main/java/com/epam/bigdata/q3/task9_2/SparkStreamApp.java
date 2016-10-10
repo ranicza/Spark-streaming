d9665271652423c84b4936b680d51f1bfbe8cab3
Maryna Maroz <maryna_maroz@epam.com>
2016-10-10 13:04:51 +0300

    streaming

--- src/main/java/com/epam/bigdata/q3/task9_2/SparkStreamApp.java
+++ src/main/java/com/epam/bigdata/q3/task9_2/SparkStreamApp.java
@@ -19,9 +19,19 @@ import org.apache.spark.streaming.api.java.JavaPairReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.KafkaUtils;
 
-
 import scala.Tuple2;
 
+/**
+ * 
+ * @author Maryna_Maroz
+ * 
+ *  Usage: SparkStreamingApp <master> <zkQuorum> <group> <topics> <numThreads> <table> <columnFamily>
+ *   <zkQuorum> is a list of one or more zookeeper servers that make quorum
+ *   <group> is the name of kafka consumer group
+ *   <topics> is a list of one or more kafka topics to consume from
+ *   <numThreads> is the number of threads the kafka consumer should use
+ *
+ */
 public class SparkStreamApp {
 	  private static final Pattern SPACE = Pattern.compile(" ");
 
@@ -29,7 +39,7 @@ public class SparkStreamApp {
 	    public static void main(String[] args) throws Exception {
 	        if (args.length == 0) {
 	            System.err
-	                    .println("Usage: SparkStreamingLogAggregationApp {master} {zkQuorum} {group} {topic} {numThreads} {table} {columnFamily}");
+	                    .println("Usage: SparkStreamingApp {master} {zkQuorum} {group} {topic} {numThreads} {table} {columnFamily}");
 	            System.exit(1);
 	        }
 
@@ -38,13 +48,12 @@ public class SparkStreamApp {
 	        String group = args[2];
 	        String[] topics = args[3].split(",");
 	        int numThreads = Integer.parseInt(args[4]);
-	        String tableName = args[5];
+	        String table = args[5];
 	        String columnFamily = args[6];
 
-	        SparkConf sparkConf = new SparkConf().setAppName("SparkStreamingLogAggregationApp");
+	        SparkConf sparkConf = new SparkConf().setAppName("SparkStreamingApp");
 
-	       // JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, new Duration(2000));
-	        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, new Duration(500));
+	        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, new Duration(2000));
 
 	        Map<String, Integer> topicMap = new HashMap<>();
 	        for (String topic : topics) {
@@ -58,7 +67,7 @@ public class SparkStreamApp {
 	            @Override
 	            public String call(Tuple2<String, String> tuple2) {
 
-	                System.out.println("###1 " + tuple2.toString());
+	                System.out.println("#1 " + tuple2.toString());
 	                return tuple2._2();
 	            }
 	        });
@@ -82,6 +91,7 @@ public class SparkStreamApp {
 	                return i1 + i2;
 	            }
 	        });
+	      //  wordCounts.saveAsNewAPIHadoopFiles(prefix, suffix);
 
 	        wordCounts.print();
 	        jssc.start();
