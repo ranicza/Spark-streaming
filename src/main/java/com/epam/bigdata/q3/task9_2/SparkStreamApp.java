88427e481e412bfe8cffa0537bf0301d4532a175
Maryna Maroz <maryna_maroz@epam.com>
2016-10-10 15:46:40 +0300

    add configs

--- src/main/java/com/epam/bigdata/q3/task9_2/SparkStreamApp.java
+++ src/main/java/com/epam/bigdata/q3/task9_2/SparkStreamApp.java
@@ -201,8 +201,8 @@ public class SparkStreamApp {
 	        try {
 	               config = HBaseConfiguration.create();
 	               config.set("hbase.zookeeper.quorum", "127.0.0.1");
-	               //config.set("hbase.zookeeper.property.clientPort","2181");
-	               //config.set("hbase.master", "127.0.0.1:60000");
+	               config.set("hbase.zookeeper.property.clientPort","2181");
+	               config.set("hbase.master", "127.0.0.1:60000");
 	               HBaseAdmin.checkHBaseAvailable(config);
 	               System.out.println("HBase is running!");
 	             } 
